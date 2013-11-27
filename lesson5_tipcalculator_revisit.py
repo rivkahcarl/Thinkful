ea40c325438017816f06fc251a2771b1f6dd8e81
Rivkah Carl <rivkah@noodle.org>
2013-11-27 09:26:28 -0800

    using sys.argv

--- lesson5_tipcalculator_revisit.py
+++ lesson5_tipcalculator_revisit.py
@@ -21,13 +21,34 @@
 #print text
 
 ##raw input method
-meal = raw_input("The base of my meal is: ")
-tax_percent = raw_input("What is the percentage of tax at the restaurant? Please enter in decimal format: ")
+
+#meal = raw_input("The base of my meal is: ")
+#tax_percent = raw_input("What is the percentage of tax at the restaurant? Please enter in decimal format: ")
+#tax= float(tax_percent)* float(meal)
+#pre_tip = float(meal) + float(tax)
+#tip = raw_input("Please enter the percent of tip you would like to add? (in decimal format): ")
+#tip_tot= float(tip)*pre_tip 
+#total=  tip_tot + pre_tip
+
+#text = "The base cost of your meal was $%s \n You need to pay $%s for tax.\n Tipping at a rate of %s, you should leave $%s for the tip.\n The grand total of your meal is $%s" % (meal, tax, tip, tip_tot, total)
+#print text
+
+##user supplied variables with sys.argv
+##arguments go directly in command line
+
+import sys
+
+meal=sys.argv[1]
+tax_percent= sys.argv[2]
+tip = sys.argv[3]
 tax= float(tax_percent)* float(meal)
 pre_tip = float(meal) + float(tax)
-tip = raw_input("Please enter the percent of tip you would like to add? (in decimal format): ")
 tip_tot= float(tip)*pre_tip 
 total=  tip_tot + pre_tip
 
 text = "The base cost of your meal was $%s \n You need to pay $%s for tax.\n Tipping at a rate of %s, you should leave $%s for the tip.\n The grand total of your meal is $%s" % (meal, tax, tip, tip_tot, total)
 print text
+
+
+
+
