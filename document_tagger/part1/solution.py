03f53fa25d2b184a49c5f28e408ba16503ffbab7
Rivkah Carl <rivkah@noodle.org>
2013-12-10 08:28:24 -0800

    added lesson 7 and document tagger folder

--- document_tagger/part1/solution.py
+++ document_tagger/part1/solution.py
@@ -1,8 +1,17 @@
 import re
+import sys
 from pg_sample_texts import DIV_COMM, MAG_CART
  
 documents = [DIV_COMM, MAG_CART]
- 
+
+# first we need to do something with the user supplied keywords
+# which we're getting with sys.argv. Remember, the script name itself
+# is at index 0 in sys.argv, so we'll slice everything from index 1 forward.
+searches = {}
+for kw in sys.argv[1:]:
+  searches[kw] = re.compile(r'\b' + kw + r'\b', re.IGNORECASE)
+
+
 # PREPARE OUR REGEXES FOR METADATA SEARCHES #
 # we'll use re.compile() here, which allows you to assign a regex pattern
 # to a variable. We'll do this for each our metadata fields.
@@ -46,3 +55,16 @@ for i, doc in enumerate(documents):
   print "Translator(s): {}".format(translator)
   print "Illustrator(s): {}".format(illustrator)
   print "\n"
+
+  print "***" * 25
+  print "Here's the keyword info for doc {}:".format(i)
+  for search in searches:
+    print "\"{0}\": {1}".format(search, len(re.findall(searches[search], doc)))
+
+
+
+ 
+
+ 
+
+  
