5b59bdfe4d6127b453a267aa8f74967de9ac41db
Rivkah Carl <rivkah@noodle.org>
2013-12-30 19:23:16 -0800

    changed class object

--- Unit2/pickle_tester/pickler.py
+++ Unit2/pickle_tester/pickler.py
@@ -1,16 +1,13 @@
-import sys
-import os
-import pickle
-import unittest
+class pickler(object):
 
-def list_pickler():
-    list1 = [1, 2, 3]
-    output = open('test.p', 'wb')
-    pickle.dump(list1, output)
-    output.close()
+    def list_pickler(self, list1):
+        list1 = [1, 2, 3]
+        output = open('test.p', 'wb')
+        pickle.dump(list1, output)
+        output.close()
 
 
-def unpickler():
-    picklefile = open('test.p', 'rb')
-    list1 = pickle.load(picklefile)
-    picklefile.close()
+    def unpickler(self, list1):
+        picklefile = open('test.p', 'rb')
+        list1 = pickle.load(picklefile)
+        picklefile.close()
