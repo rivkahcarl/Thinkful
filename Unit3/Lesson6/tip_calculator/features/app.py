aa1cfc67bc0846f4c6b3245d58410400cbe4ed33
Rivkah Carl <rivkah@noodle.org>
2014-03-23 16:12:55 -0700

    calc mid

--- Unit3/Lesson6/tip_calculator/features/app.py
+++ Unit3/Lesson6/tip_calculator/features/app.py
@@ -10,6 +10,18 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 @app.route('/results', methods=['POST'])
+def calculate_rate(base, percentage):
+    return base * percentage
+
+def calculate_tip(meal_base, tip_rate):
+     """
+    Calculates dollar amounts for tip using base meal cost
+    """
+    meal_base = 
+    tip_value = calculate_rate(meal_base, tip_rate)
+    return(tip_value)
+    
+
 def results():
   return render_template('results.html')
 
