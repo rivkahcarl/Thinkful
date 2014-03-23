08776c90d3740275be75ed301f1fed3a71a21140
Rivkah Carl <rivkah@noodle.org>
2014-03-23 15:59:34 -0700

    added BDD calculator beginning

--- Unit3/Lesson6/tip_calculator/features/steps/calculator.py
+++ Unit3/Lesson6/tip_calculator/features/steps/calculator.py
@@ -24,7 +24,15 @@ def step_impl(context):
 @when(u'I enter a meal cost of $50 and a 20% tip')
 def step_impl(context):
     br = context.browser
+    br.get('http://localhost:5000')
+    meal_cost = br.find_element_by_name("meal_cost")
+    meal_cost.send_keys("50")
+    tip_percentage = br.find_element_by_name("tip_percentage")
+    tip_percentage.send_keys("20")
+    br.find_element_by_id("submit").click()
 
 @then(u'I should see it calculated to $10 as the tip amount')
 def step_impl(context):
     br = context.browser
+    tip_amount = br.find_element_by_id('results')
+    assert tip_amount == "10"
